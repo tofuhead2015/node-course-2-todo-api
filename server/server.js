b3cf5c1d8adfc7e5cb49ef058b87a84488b87eda
Kai <tofuhead@gmail.com>
2018-09-11 09:52:28 -0400

    added db and models folders

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const mongoose = require('./db/mongoose.js').mongoose
+const Todo = require('./models/todo').Todo
+const {User} = require('./models/user')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res)=>{
+    const todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then((doc)=>{
+        res.send(doc)
+    }, e=>{
+        res.status(400).send(e)
+    })
+})
+
+app.listen(4000, ()=>{
+    console.log('Started on port 4000')
+})
