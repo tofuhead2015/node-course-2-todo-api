19316892402191ddb541ae9bffd9e4f26bfbe83a
Kai <tofuhead@gmail.com>
2018-09-17 11:54:45 -0400

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,41 @@ describe('GET /todos/:id', ()=>{
         .end(done)               
     })
 })
+
+describe('DELETE /todos/:id', ()=>{
+    it('should remove a todo', (done)=>{
+        const hexId = todos[1]._id.toHexString()
+
+        request(app)
+        .delete('/todos/' + hexId)
+        .expect(200)
+        .expect(res=>{
+            expect(res.body.todo._id).toBe(hexId)
+        })
+        .end((err, res)=>{
+            if (err){
+                return done(err)
+            }
+            Todo.findById(hexId).then((todo)=>{
+                expect(todo).toBe(null)              
+                done()
+            }).catch(e=>{
+                done(e)
+            })
+        })
+    })
+
+    it('should return 404 if todo not found', (done)=>{
+        request(app)
+        .delete('/todos/' + new ObjectID().toHexString)        
+        .expect(404)        
+        .end(done)  
+    })
+
+    it('should return 404 if object id is invalid', (done)=>{
+        request(app)
+        .delete('/todos/5b9a6466df82b04310267a4a1')        
+        .expect(404)        
+        .end(done) 
+    })
+})
