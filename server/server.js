f69cf955d4e2489372a08a08d66f2e7a025e7017
Kai <tofuhead@gmail.com>
2018-09-18 08:39:04 -0400

    add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const mongoose = require('./db/mongoose.js').mongoose
 const Todo = require('./models/todo').Todo
 const {User} = require('./models/user')
@@ -60,6 +61,30 @@ app.delete('/todos/:id', (req, res)=>{
     })
 })
 
+app.patch('/todos/:id', (req, res)=>{
+    const id = req.params.id
+    const body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime()        
+    } else {
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo=>{
+        if (!todo) {
+            return res.status(404).send()
+        }
+        res.send({todo})        
+    }).catch (e=>{
+        res.status(400).send()
+    })
+})
 app.listen(port, ()=>{
     console.log('Started on port ' + port)
 })
